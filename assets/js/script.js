f630f128b97068c50709f45a885116b98232de5e
Tretiakoff <c.tretiakoff@gmail.com>
2016-02-25 02:25:02 +0100

    fix font

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    document.querySelectorAll('.sign-up')[0].onclick = function () {
+        window.open('#register', '_self');
+    };
+
+    document.querySelectorAll('.register')[0].onclick = function () {
+        window.open('#register', '_self');
+    };
+
+    document.querySelectorAll('.more')[0].onclick = function () {
+        window.open('#more', '_self');
+    };
+};
