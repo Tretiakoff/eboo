eedadb7f71b6573be5b997131d973cf5fd2b66b8
Tretiakoff <c.tretiakoff@gmail.com>
2016-02-25 10:23:40 +0100

    Functionnal appstores buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,4 +22,12 @@ $(function () {
     document.querySelectorAll('.next')[0].onclick = function () {
         smoothScroll('#project');
     };
+
+    document.querySelectorAll('.google-play')[0].onclick = function () {
+        window.open('https://play.google.com/store?hl=fr', '_blank');
+    };
+
+    document.querySelectorAll('.app-store')[0].onclick = function () {
+        window.open('https://itunes.apple.com/fr/app/apple-store/id375380948?mt=8', '_blank');
+    };
 });
